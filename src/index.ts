5de240c678aed593bc5d71ef081a9d1fc94f8f11
dumitru_pascla <dumitru.pascal@vitamojo.com>
2023-03-06 18:23:15 +0200

    Solid practice

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,188 @@
-// import { employee } from "./single-responsiblity";
-// import "./open-closed";
-// import "./interface-segregation";
+// Person should take and put any item from closet
+// Person should write on paper using Biro
+
+interface IItem {
+  name: string
+}
+
+interface IBiro extends IItem {
+  decorateText(text: string): string;
+  write(text: string, paper: IPaper): IPaper;
+}
+
+interface IPaper extends IItem {
+  text: Array<{text:string, wroteWith: IBiro}>;
+  write(text: string, item: IBiro): IPaper;
+  read():string;
+}
+
+interface ICloset {
+  items: Array<IItem>;
+
+  watchAllItems(): Array<IItem>;
+  insertItem(item: IItem): void;
+  getItem(itemName: string): IItem | undefined;
+}
+
+
+interface IPerson {
+  name: string;
+  items: Array<IItem>;
+
+  write(text: string): IPaper;
+
+  lookInClosed(closed: ICloset): Array<IItem>;
+  takeItem(closed: ICloset, itemName: string): void;
+  putItem(closed: ICloset, itemName: string): void;
+}
+
+// Items
+
+class Biro implements IBiro {
+  public name = "biro";
+
+  write(text: string, paper: IPaper): IPaper {
+    return paper.write(text, this);
+  }
+  decorateText(text: string): string {
+    throw new Error("Method not implemented.");
+  }
+}
+class Pencil extends Biro {
+  public name = "pencil";
+  decorateText(text: string): string {
+    return `<pencil>${text}</pencil>\n`;
+  }
+}
+class Pen extends Biro {
+  public name = "pen";
+  decorateText(text: string): string {
+    return `<pen>${text}</pen>\n`;
+  }
+}
+
+
+class Paper implements IPaper {
+  public name = "paper";
+  public text: Array<{text:string, wroteWith: IBiro}> = [];
+
+  write(text: string, wroteWith: IBiro): IPaper {
+    this.text.push({ text, wroteWith });
+
+    return this;
+  }
+
+  read(){
+   return  this.text.reduce((acc,{text, wroteWith})=> {
+      acc += wroteWith.decorateText(text);
+      return acc;
+    },'')
+  }
+}
+
+class Pocket implements IItem {
+  public name = "pocket";
+}
+
+// Closet
+class Closet implements ICloset {
+  constructor(public items: Array< IItem>){}
+
+  watchAllItems(): IItem[] {
+    return this.items
+  }
+
+  insertItem(item: IItem): void {
+     this.items.push(item)
+  }
+
+  getItem(itemName: string): IItem | undefined {
+    return this.items.find((i) => i.name === itemName);
+  }
+}
+
+// Person
+class Person implements IPerson {
+  items: IItem[] = [];
+
+  constructor(public name: string) {}
+
+  lookInClosed(closed: ICloset): IItem[] {
+    return closed.watchAllItems();
+  }
+
+  write(text: string): IPaper {
+    const paper = this.items.find((item) => item instanceof Paper);
+
+    if (!paper){
+      throw new Error("No paper was taken");
+    }
+    const biro = this.items.find((item) => item instanceof Biro);
+
+    if (!biro) {
+      throw new Error("No biro was taken");
+    }
+
+    return (paper as Paper).write(text, biro as Biro);
+  }
+
+  takeItem(closed: ICloset, itemName: string): void {
+    const item = closed
+      .watchAllItems()
+      .find((item) =>item.name === itemName);
+
+
+    if (!item){
+      throw new Error("This item is not in Closet");
+    }
+
+    this.items.push(item);
+  }
+
+  putItem(closed: ICloset, itemName: string): void {
+    const itemIndex = this.items.findIndex((item) => item.name === itemName);
+
+    if (itemIndex===-1){
+      throw new Error("User don't have such item");
+    }
+
+    closed.insertItem(this.items.splice(itemIndex, 1)[0]);
+  }
+}
+
+
+// How it works
+const person1 = new Person('Dima')
+const closed1 = new Closet([
+  new Pencil(),
+  new Pencil(),
+  new Pencil(),
+  new Pen(),
+  new Pocket(),
+  new Paper(),
+]);
+
+
+console.log('that is inside',  person1.lookInClosed(closed1));;
+
+person1.takeItem(closed1, 'paper')
+person1.takeItem(closed1, 'pen')
+
+
+person1.write('Hello world')
+
+person1.putItem(closed1, "pen");
+person1.takeItem(closed1, "pencil");
+
+person1.write("Hello again");
+
+person1.putItem(closed1, "paper");
+
+
+const paper = closed1.getItem('paper')
+
+if(paper){
+  const text = (paper as IPaper).read()
+
+  console.log(text);
+}
--- src/interface-segregation/index.ts
+++ src/interface-segregation/index.ts
@@ -1,9 +1,60 @@
-// // Wong
-// interface IDatabase  {
-//   getUsers(): Array<any>
-//   updateUser(data:any): Array<any>
-//   deleteUser(data:any): boolean
-//   connect(): void
+// // // Wong
+// // interface IDatabase  {
+// //   getUsers(): Array<any>
+// //   updateUser(data:any): Array<any>
+// //   deleteUser(data:any): boolean
+// //   connect(): void
+// // }
+
+// // class Database implements IDatabase {
+// //   connect(): void {
+// //     throw new Error("Method not implemented.");
+// //   }
+// //   getUsers(): any[] {
+// //     throw new Error("Method not implemented.");
+// //   }
+// //   updateUser(data: any): any[] {
+// //     throw new Error("Method not implemented.");
+// //   }
+// //   deleteUser(data: any): boolean {
+// //     throw new Error("Method not implemented.");
+// //   }
+// // }
+
+// // class Manager {
+// //   constructor(readonly db:IDatabase){}
+
+// //   getAll() {
+// //     return this.db.getUsers();
+// //   }
+// //   update() {
+// //     return this.db.updateUser({});
+// //   }
+// //   delete() {
+// //     return this.db.deleteUser({});
+// //   }
+// // }
+
+// // // Wrong as we send entire interface but in that case we need interface just with limited functionality
+// // class Sellers {
+// //   constructor(readonly db: IDatabase) {}
+// //   getAll() {
+// //     return this.db.getUsers();
+// //   }
+// // }
+
+// // Correct
+// interface IReadDatabase {
+//   getUsers(): Array<any>;
+// }
+// interface IWriteDatabase {
+//   updateUser(data: any): Array<any>;
+//   deleteUser(data: any): boolean;
+// }
+
+
+// interface IDatabase extends IReadDatabase, IWriteDatabase {
+//   connect(): void;
 // }
 
 // class Database implements IDatabase {
@@ -22,7 +73,7 @@
 // }
 
 // class Manager {
-//   constructor(readonly db:IDatabase){}
+//   constructor(readonly db: IReadDatabase & IWriteDatabase) {}
 
 //   getAll() {
 //     return this.db.getUsers();
@@ -35,61 +86,10 @@
 //   }
 // }
 
-// // Wrong as we send entire interface but in that case we need interface just with limited functionality
+// // In that case Sellers know just that we can read users and nothing more, which is not related to their implementation
 // class Sellers {
-//   constructor(readonly db: IDatabase) {}
+//   constructor(readonly db: IReadDatabase) {}
 //   getAll() {
 //     return this.db.getUsers();
 //   }
 // }
-
-// Correct
-interface IReadDatabase {
-  getUsers(): Array<any>;
-}
-interface IWriteDatabase {
-  updateUser(data: any): Array<any>;
-  deleteUser(data: any): boolean;
-}
-
-
-interface IDatabase extends IReadDatabase, IWriteDatabase {
-  connect(): void;
-}
-
-class Database implements IDatabase {
-  connect(): void {
-    throw new Error("Method not implemented.");
-  }
-  getUsers(): any[] {
-    throw new Error("Method not implemented.");
-  }
-  updateUser(data: any): any[] {
-    throw new Error("Method not implemented.");
-  }
-  deleteUser(data: any): boolean {
-    throw new Error("Method not implemented.");
-  }
-}
-
-class Manager {
-  constructor(readonly db: IReadDatabase & IWriteDatabase) {}
-
-  getAll() {
-    return this.db.getUsers();
-  }
-  update() {
-    return this.db.updateUser({});
-  }
-  delete() {
-    return this.db.deleteUser({});
-  }
-}
-
-// In that case Sellers know just that we can read users and nothing more, which is not related to their implementation
-class Sellers {
-  constructor(readonly db: IReadDatabase) {}
-  getAll() {
-    return this.db.getUsers();
-  }
-}
